3a3bdf55979350b63ac96883c87b7abd96d04106
SMA <sandra.mejia.av@gmail.com>
2019-07-03 14:27:14 -0500

    contaminant api

--- flask project/app.py
+++ flask project/app.py
@@ -1,21 +1,166 @@
 from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
-
+from pymongo import MongoClient
+from flask import request
+import json
+import pandas as pd
+from flask import jsonify
 
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/Project")
-
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/Project")
+connectionString = "mongodb://dbAdmin:Vol8e3v5XLGYrwTK@cluster0-shard-00-00-0dend.mongodb.net:27017,cluster0-shard-00-01-0dend.mongodb.net:27017,cluster0-shard-00-02-0dend.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+dbClient = MongoClient(connectionString)
+db = dbClient["cdmx_pollution"]
 
 # Route to render index.html template using data from Mongo
 
 @app.route("/")
 def home():
     return render_template("index.html")
+   
+@app.route("/contaminants/")
+def data1():
+        _args = verifyQueryParameters(request.args)
+        docs = getContaminants(**_args)
+        df = pd.DataFrame(list(docs))
+        print(df.head(5))
+        result = {
+            'data':  json.loads(df.to_json(orient='records'))
+                # 'data': {}
+        }
+        return jsonify(result)
+
+
+def getContaminants(match, project = {}, skip = 0, limit = 100, orient='records'):
+        _pipeline = [
+            {
+                '$match': {
+                    **match
+                }
+            },
+            {
+                '$project': {
+                        '_id': 0
+                }
+            }, 
+            {
+                '$sort': {
+                    'date': 1
+                }
+            }, 
+            {
+                '$skip': skip
+            }, 
+            {
+                '$limit': limit
+            }            
+        ]
+        print(_pipeline)
+        if len(project) > 0:
+            _pipeline.append({'$project': { **project }})
+        docs = db.contaminants2016.aggregate(_pipeline)
+        # for doc in docs:
+                # print(doc)
+        return docs
+
+
+
+
+
+
+# -----------------
+@app.route("/emergencies/")
+def data2():
+        _args = verifyQueryParameters(request.args)
+        docs = getEmergencies(**_args)
+        df = pd.DataFrame(list(docs))
+        print(df.head(5))
+        result = {
+            'data':  json.loads(df.to_json(orient='records'))
+                # 'data': {}
+        }
+        return jsonify(result)
+
+
+def getEmergencies(match, project = {}, skip = 0, limit = 100, orient='records'):
+        _pipeline = [
+            {
+                '$match': {
+                    **match
+                }
+            },
+            {
+                '$project': {
+                        '_id': 0
+                }
+            }, 
+            {
+                '$sort': {
+                    'date': 1
+                }
+            }, 
+            {
+                '$skip': skip
+            }, 
+            {
+                '$limit': limit
+            }            
+        ]
+        print(_pipeline)
+        if len(project) > 0:
+            _pipeline.append({'$project': { **project }})
+        docs = db.respiratorias2016.aggregate(_pipeline)
+        # for doc in docs:
+                # print(doc)
+        return docs
+# -----------------
+
+
+
+
+
+
+
+
+
+
+
+def verifyQueryParameters(args):
+    _args = { }
+    parametersAllowed = [
+        'q',
+        'project',
+        'skip',
+        'limit',
+        'orient'
+    ]
+    if args.get('q'):
+        qDict = json.loads(args.get('q'))
+        if len(qDict) == 0:
+            raise Exception("q parameter can't be empty")
+        _args['match'] = qDict
+
     
+    if args.get('project'):
+        _args['project'] = json.loads(args.get('project'))
+    
+    if args.get('skip'):
+        _args['skip'] = json.loads(args.get('skip'))
+
+    if args.get('limit'):
+        _args['limit'] = json.loads(args.get('limit'))
+
+    if args.get('orient'):
+        _args['orient'] = json.loads(args.get('orient'))
+    else:
+        _args['orient'] = 'records'
+
+    return _args
+
 @app.route("/data")
 def stock():
         #print("Municipality")
--- flask project/static/js/heatmap.js
+++ flask project/static/js/heatmap.js
@@ -1,67 +1,140 @@
-let heatLayer = undefined;
+// ----------------------------------------------------
+// | Contaminant and Emergencies Heatmaps Base Layers |
+// ----------------------------------------------------
 
-// Base map
-var myMap = new L.Map('map', {
-  center: new L.LatLng(19.39068, -99.2837),
-  zoom: 11
+let contaminantHeatLayer = undefined;
+let emergenciesHeatLayer = undefined;
+
+
+// Create an array with formatted 2016 date strings
+function appendLeadingZeroes(n) {
+    if (n <= 9) {
+        return "0" + n;
+    }
+    return n
+}
+var daysOfYear = []
+for (var d = new Date('01/01/2016'); d <= new Date(2016, 11, 31); d.setDate(d.getDate() + 1)) {
+    var dateString = appendLeadingZeroes(d.getDate()) + "/" + appendLeadingZeroes(d.getMonth() + 1) + "/" + d.getFullYear()
+    daysOfYear.push(dateString);
+}
+
+
+// Contaminant base map
+var contaminantMap = new L.Map('contaminantMap', {
+    center: new L.LatLng(19.4196797, -99.1270159),
+    zoom: 11
 });
+var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(contaminantMap);
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
+// Emergencies base map
+var emergenciesMap = new L.Map('emergenciesMap', {
+    center: new L.LatLng(19.4196797, -99.1270159),
+    zoom: 11
+});
+var baseLayer = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(emergenciesMap);
+
 
 // ----------
 // Initial map with O3 at 01/01/2016
 var init = () => {
-  getData("O3", '01/01/2016');
+    getData("O3", '01/01/2016');
 }
 
 // Update map
-var createLayer = (selectedContaminant, selectedDate) => {
-  d3.csv("../static/res/contaminants2016.csv", function (error, contaminants) {
-    if (error) throw error;
-    var cont = contaminants;
-
-
-    var graphContaminant = []
-    for (let i = 0; i < cont.length; i++) {
-      if (cont[i].date === selectedDate) {
-        if (cont[i].id_parameter === selectedContaminant) {
-          graphContaminant.push(cont[i])
+// -----------------------
+// | Contaminant Heatmap |
+// -----------------------
+const createContaminantLayer = async (selectedContaminant, selectedDate) => {
+    const url = `/contaminants/?q={"id_parameter":"${selectedContaminant}","date":"${selectedDate}"}`;
+    d3.json(url, function (contaminants) {
+        var graphContaminant = contaminants.data;
+        // debugger;
+        var contaminantHeatArray = [];
+        for (let j = 0; j < graphContaminant.length; j++) {
+            var value = graphContaminant[j].value;
+            if (value) {
+                contaminantHeatArray.push([graphContaminant[j].latitud, graphContaminant[j].longitud, graphContaminant[j].value]);
+            }
         }
-      }
-    }
-
-    var heatArray = [];
-    for (let j = 0; j < graphContaminant.length; j++) {
-      var value = graphContaminant[j].value;
-      if (value) {
-        heatArray.push([graphContaminant[j].latitud, graphContaminant[j].longitud, graphContaminant[j].value]);
-      }
-    }
-
-    if (heatLayer !== undefined) {
-      myMap.removeLayer(heatLayer);
-
-    }
-
-    heatLayer = L.heatLayer(heatArray, {
-      radius: 50,
-      blur: 35
-    }).addTo(myMap);
-
-
-  });
+        if (contaminantHeatLayer !== undefined) {
+            contaminantMap.removeLayer(contaminantHeatLayer);
+        }
+        contaminantHeatLayer = L.heatLayer(contaminantHeatArray, {
+            radius: 50,
+            blur: 45,
+            opacity: 0.5
+        }).addTo(contaminantMap);
+    });
 }
+
+
+
+// Update map
+// -----------------------
+// | Emergencies Heatmap |
+// -----------------------
+const createEmergenciesLayer = async (selectedDate) => {
+    const url = `/emergencies/?q={"date":"${selectedDate}"}`;
+    d3.json(url, function (emergencies) {
+        var graphEmergencies = emergencies.data;
+        // debugger;
+        var emergenciesHeatArray = [];
+        for (let j = 0; j < graphEmergencies.length; j++) {
+            var value = graphEmergencies[j].value;
+            if (value) {
+                emergenciesHeatArray.push([graphEmergencies[j].latitud, graphEmergencies[j].longitud, graphEmergencies[j].value]);
+            }
+        }
+        if (emergenciesHeatLayer !== undefined) {
+            emergenciesMap.removeLayer(emergenciesHeatLayer);
+        }
+        emergenciesHeatLayer = L.heatLayer(emergenciesHeatArray, {
+            radius: 50,
+            blur: 45,
+            opacity: 0.5
+        }).addTo(emergenciesMap);
+    });
+}
+
+
+
 
 
 // ---------
 // contaminant selection
 function getData(selCont) {
-  createLayer(selCont, '01/01/2016');
+    createContaminantLayer(selCont, '01/01/2016');
+    createEmergenciesLayer("01/01/2016")
 }
 
+
+
+
+for (let i = 0; i < daysOfYear.length; i++) {
+    var pollutant = 'O3'
+    var date = daysOfYear[i];
+    myVar = setTimeout(function () {
+        createContaminantLayer(pollutant, date);
+        createEmergenciesLayer(date);
+    }, 3000);
+    console.log(i)
+}
+
+
+
 init();
+
+
+
+
